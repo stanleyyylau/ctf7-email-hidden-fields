175ce80fed4dd26201b5973505266baa7126114b
Stanley <stanleyyylau@gmail.com>
2021-06-18 15:56:57 +0800

    update updater

--- ctf7-email-hidden-fields.php
+++ ctf7-email-hidden-fields.php
@@ -84,15 +84,18 @@ run_ctf7_email_hidden_fields();
 /*
  * Github Updater
  */
-include_once('updater.php');
+if ( ! defined( 'ABSPATH' ) || class_exists( 'WPGitHubUpdater' ) || class_exists( 'WP_GitHub_Updater' ) ) {
+} else {
+    require_once plugin_dir_path( __FILE__ ) . 'updater.php';
+}
 if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
     $config = array(
         'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-        'proper_folder_name' => 'ctf7-email-hidden-fields', // this is the name of the folder your plugin lives in
-        'api_url' => 'https://api.github.com/repos/stanleyyylau/ctf7-email-hidden-fields', // the GitHub API url of your GitHub repo
-        'raw_url' => 'https://raw.github.com/stanleyyylau/ctf7-email-hidden-fields/master', // the GitHub raw url of your GitHub repo
-        'github_url' => 'https://github.com/stanleyyylau/ctf7-email-hidden-fields', // the GitHub url of your GitHub repo
-        'zip_url' => 'https://github.com/stanleyyylau/ctf7-email-hidden-fields/zipball/master', // the zip url of the GitHub repo
+        'proper_folder_name' => 'anton-roles', // this is the name of the folder your plugin lives in
+        'api_url' => 'https://api.github.com/repos/stanleyyylau/anton-roles', // the GitHub API url of your GitHub repo
+        'raw_url' => 'https://raw.github.com/stanleyyylau/anton-roles/master', // the GitHub raw url of your GitHub repo
+        'github_url' => 'https://github.com/stanleyyylau/anton-roles', // the GitHub url of your GitHub repo
+        'zip_url' => 'https://github.com/stanleyyylau/anton-roles/zipball/master', // the zip url of the GitHub repo
         'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
         'requires' => '5.7', // which version of WordPress does your plugin require?
         'tested' => '5.7', // which version of WordPress is your plugin tested up to?
